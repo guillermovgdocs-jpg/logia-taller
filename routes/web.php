0075a29f7f96930e291462b995b416edc5047332
Guillermo <guillermovg.docs@gmail.com>
2025-09-05 09:39:05 -0600

    Portada 2

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
+    return "hola mundo";
 })->name('home');
 
 Route::get('dashboard', function () {
